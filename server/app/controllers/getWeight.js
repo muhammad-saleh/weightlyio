df71747bb9ffe158a48fc0f0120820300dce0f84
Mohamed Saleh <eng.m.ahmed@live.com>
2016-03-01 08:44:37 +0200

    Some code clean up

--- server/app/controllers/getWeight.js
+++ server/app/controllers/getWeight.js
@@ -1,16 +1,16 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-
 const mongoose = require('mongoose');
-var Weight = require('../models/weight');
+const Weight = require('../models/weight');
 
-module.exports = function(req, res){
-    // res.json("Hi Get Weight")
-    Weight.find({user: req.user._id.toString()}, function (err, weights) {
-      if (err) return console.error(err);
-      res.json(weights)
+module.exports = function (req, res) {
+    Weight.find({
+        user: req.user._id.toString()
+    }, function (err, weights) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(weights)
+        }
     })
 }
--- server/app/controllers/postWeight.js
+++ server/app/controllers/postWeight.js
@@ -1,30 +1,23 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-
 const mongoose = require('mongoose');
-var Weight = require('../models/weight');
+const Weight = require('../models/weight');
 
-module.exports = function(req, res){
-    console.log(req.user._id)
+module.exports = function (req, res) {
+    var weight = new Weight({
+        weight: req.body.weight.toString(),
+        date: new Date(),
+        user: req.user._id.toString(),
+        feel: req.body.feel || "normal"
+    });
 
-    // var weight = new WeightSchema();      // create a new instance of the Bear model
-    //     weight.weight = req.body.weight;  // set the bears name (comes from the request)
-
-        var weight = new Weight({
-            weight:req.body.weight.toString(),
-            date: new Date(),
-            user: req.user._id.toString(),
-            feel: req.body.feel || "normal"
-        });
-
-        // save the bear and check for errors
-        weight.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Weight created!' });
-        });
+    weight.save(function (err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json({
+                message: 'Weight created!'
+            });
+        }
+    });
 }
