3199ce2da12e79cadc2a316fb898e144be77b69c
Mohamed Saleh <eng.m.ahmed@live.com>
2016-03-05 06:26:34 +0200

    Initial commit for the action types constants file

---
+++ client/src/js/constants/actionTypes.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const ActionTypes = {
+    "ADD_GOAL": "ADD_GOAL",
+    "GET_WEIGHT": "GET_WEIGHT",
+    "ADD_WEIGHT": "ADD_WEIGHT",
+    "DELETE_WEIGHT": "DELETE_WEIGHT"
+}
+
+export default ActionTypes;
