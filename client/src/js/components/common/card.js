e3ca42d519009a45d9869e1235652edd77ac3d7e
Mohamed Saleh <eng.m.ahmed@live.com>
2016-03-08 07:54:42 +0200

    Adjustments in the card component

--- client/src/js/components/common/card.js
+++ client/src/js/components/common/card.js
@@ -6,12 +6,24 @@ import FontAwesome from 'react-fontawesome';
 export default class Card extends React.Component {
     render() {
         let cardIcon = '';
+        let cardTitle = '';
+        let cardHeader = '';
+
         if(this.props.cardIcon && this.props.cardIcon.trim().length > 0){
             cardIcon = <FontAwesome name={this.props.cardIcon} />
         }
+
+        if(this.props.cardTitle && this.props.cardTitle.trim().length > 0){
+            cardTitle = <h4>{cardIcon} {this.props.cardTitle}</h4>
+        }
+
+        if(cardTitle !== ''){
+            cardHeader = <div className="cardTitle">{cardTitle}</div>
+        }
+
         return (
             <div className={this.props.cssClass + ' card'}>
-                <div className="cardTitle"><h4>{cardIcon} {this.props.cardTitle}</h4></div>
+                {cardHeader}
                 {this.props.children}
             </div>
         )
