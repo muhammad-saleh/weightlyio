cb84e9864b90dca29f53ac64c2f0f6f4406e474e
Mohamed Saleh <eng.m.ahmed@live.com>
2016-03-28 05:50:47 +0200

    [client] Added time picker and fixed the add weight functionality after adding auth0

--- client/src/js/components/AddWeight.js
+++ client/src/js/components/AddWeight.js
@@ -4,15 +4,19 @@ import AppActions from '../actions/AppActions';
 
 import Dialog from 'material-ui/lib/dialog';
 import DatePicker from 'material-ui/lib/date-picker/date-picker';
+import TimePicker from 'material-ui/lib/time-picker/time-picker';
 import RaisedButton from 'material-ui/lib/raised-button';
 import TextField from 'material-ui/lib/text-field';
 import FlatButton from 'material-ui/lib/flat-button';
 import FontAwesome from 'react-fontawesome';
+import moment from 'moment';
 
 class AddWeight extends React.Component {
     componentWillMount() {
         this.state = {
             open: false,
+            dateField: new Date(),
+            timeField: new Date()
         }
     }
 
@@ -22,13 +26,19 @@ class AddWeight extends React.Component {
 
     handleClose = () => {
         this.setState({open: false});
-        if(this.state && this.state.newWeight && this.state.newWeightDate){
-            AppActions.postWeight({weight: this.state.newWeight, date: this.state.newWeightDate})
+        if(this.state && this.state.newWeight && this.state.newWeightDate && this.state.newWeightTime){
+            AppActions.postWeight({weight: this.state.newWeight, date: moment(this.state.newWeightDate+" "+this.state.newWeightTime).toDate()})
         }
     };
 
     handleDateChange = (event, date) => {
-        this.setState({newWeightDate: date});
+        this.setState({dateField: date});
+        this.setState({ newWeightDate: moment(date).format("YYYY-MM-DD") });
+    };
+
+    handleTimeChange = (event, time) => {
+        this.setState({timeField: time});
+        this.setState({newWeightTime: moment(time).format("HH:mm") });
     };
 
     handleNewWeightChange = (event) => {
@@ -50,8 +60,9 @@ class AddWeight extends React.Component {
               open={this.state.open}
               onRequestClose={this.handleClose}
             >
-                  <DatePicker floatingLabelText="Date" hintText="Date Picker" onChange={this.handleDateChange} />
-                  <TextField floatingLabelText="Weight" hintText="Weight in Kilograms" onChange={this.handleNewWeightChange} />
+                <DatePicker floatingLabelText="Date" hintText="Date Picker" value={this.state.dateField} onChange={this.handleDateChange} />
+                <TimePicker floatingLabelText="Time" hintText="Time Picker" value={this.state.timeField} onChange={this.handleTimeChange} />
+                <TextField  floatingLabelText="Weight" hintText="Weight in Kilograms" onChange={this.handleNewWeightChange} />
             </Dialog>
           </div>
         )
